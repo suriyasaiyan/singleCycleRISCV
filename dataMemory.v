9dcb8b376a80c84ee335edd5bc97ce3e51023feb
suriyasaiyan <durandal@SuriyasTUF>
2023-10-13 07:58:10 -0400

    updated dataMemory.v

--- dataMemory.v
+++ dataMemory.v
@@ -1,18 +1,26 @@
-module dataMemory(
+module dataMemory
+#(
+    parameter ADDRSIZE = 32, // No of Locations
+    WORDSIZE = 32; // size of each location
+)(
     input memRead, memWrite, clk, reset,
     input [4:0] address,
     input [31:0] writeData,
     output reg [31:0] readData
 );
 
-    reg [31:0] memory [0:1023]; //(4KB)
+    reg [WORDSIZE - 1:0] memory [0:ADDRSIZE -1]; 
     integer i;
 
+    initial 
+        for (i = 0; i < ADDRSIZE; i = i+1) 
+            memory[i] = 32'h00000000;
+
     always @(posedge clk or posedge reset) begin
+
         if (reset) begin
-            // Reset the memory to an initial state (all zeros in this case)
-            for (i = 0; i < 1024; i = i + 1) begin
-                memory[i] <= 32'h0;
+            for (i = 0; i < ADDRSIZE; i = i + 1) begin
+                memory[i] <= 32'h00000000;
             end
         end else begin
             if (memRead)
